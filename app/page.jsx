0642c47773b69fed14b9cdf0ba981caa838f708e
Luis <solarluiso@gmail.com>
2024-07-23 16:08:40 +0200

    Header, Nav and NavMobile

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import StyleGuide from "@/components/StyleGuide";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
+      <Header />
       <Hero />
       {/* <StyleGuide /> */}
       <div className="h-[4000px]"></div>
