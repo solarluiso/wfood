37bff2ea0c658079b7154eb5f42c5386c497fcdc
Luis <solarluiso@gmail.com>
2024-07-23 13:45:47 +0200

    Hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import StyleGuide from "@/components/StyleGuide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
-      <StyleGuide />
+      <Hero />
+      {/* <StyleGuide /> */}
+      <div className="h-[4000px]"></div>
     </main>
   );
 }
