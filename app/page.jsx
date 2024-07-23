8a4a5902c74c4f24e11938d592c0a9633582aeb8
Luis <solarluiso@gmail.com>
2024-07-23 20:59:08 +0200

    Menu

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import StyleGuide from "@/components/StyleGuide";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
       <Header />
       <Hero />
+      <Menu />
       {/* <StyleGuide /> */}
       <div className="h-[4000px]"></div>
     </main>
